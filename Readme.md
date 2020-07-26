2c48b22a4823362fd25c564df9d10f23e2319432
Sergey Chystiakov <anyxem@gmail.com>
2020-07-26 15:47:01 +0200

    initail commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# get_click
+
+# Installation
+
+# Development
---
+++ src/index.js
@@ -0,0 +1,43 @@
+(function get_clicks() {
+  const clicks = []; // storing click to this variable
+
+  const gclid = document.cookie._ga || null;
+  const url = window.location.origin;
+
+  /**
+   * collect and mixup needed data
+   * @param evt
+   */
+  const captureClick = (evt) => {
+    const timestamp = new Date().getTime(); // in milliseconds
+    const e = transformEventObject(evt);
+    clicks.push( combine(e, { gclid }, { url }, { timestamp }) );
+  }
+
+  /**
+   * Maps JS event object to new scheme
+   * @param evt
+   * @returns {{x: *, y: *, element: {classes: *[], id: (*|null), type: string}}}
+   */
+  const transformEventObject = (evt) => {
+    const {
+      pageX,
+      pageY,
+      target,
+    } = evt;
+
+    return {
+      x: pageX,
+      y: pageY,
+      element: {
+        id: target.id || null,
+        classes: [...target.classList],
+        type: target.tagName.toLowerCase()
+      }
+    };
+  }
+
+  const combine = (...args) => Object.assign({}, ...args);
+
+  window.addEventListener('click', captureClick);
+})();
